64cd210ac897d477177e691510c80c2ea4756636
AnthonyJr <tonyjunior5417@gmail.com>
2017-04-09 22:29:43 -0400

    commit

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,22 +1,116 @@
-import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+// import { Component } from '@angular/core';
+// import { NavController, NavParams } from 'ionic-angular';
 
-/*
-  Generated class for the Login page.
 
-  See http://ionicframework.com/docs/v2/components/#navigation for more info on
-  Ionic pages and navigation.
-*/
-@Component({
-  selector: 'page-login',
-  templateUrl: 'login.html'
-})
-export class LoginPage {
+//   Generated class for the Login page.
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {}
+//   See http://ionicframework.com/docs/v2/components/#navigation for more info on
+//   Ionic pages and navigation.
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad LoginPage');
+// @Component({
+//   selector: 'page-login',
+//   templateUrl: 'login.html'
+// })
+// export class LoginPage {
+
+//   constructor(public navCtrl: NavController, public navParams: NavParams) {}
+
+//   ionViewDidLoad() {
+//     console.log('ionViewDidLoad LoginPage');
+//   }
+
+
+
+// }
+
+import { Injectable } from '@angular/core';
+import { Http, Headers } from '@angular/http';
+import { Storage } from '@ionic/storage';
+import 'rxjs/add/operator/map';
+ 
+@Injectable()
+export class Auth {
+ 
+  public token: any;
+ 
+  constructor(public http: Http, public storage: Storage) {
+ 
   }
-
+ 
+  checkAuthentication(){
+ 
+    return new Promise((resolve, reject) => {
+ 
+        //Load token if exists
+        this.storage.get('token').then((value) => {
+ 
+            this.token = value;
+ 
+            let headers = new Headers();
+            headers.append('Authorization', this.token);
+ 
+            this.http.get('https://YOUR_HEROKU_APP.herokuapp.com/api/auth/protected', {headers: headers})
+                .subscribe(res => {
+                    resolve(res);
+                }, (err) => {
+                    reject(err);
+                }); 
+ 
+        });         
+ 
+    });
+ 
+  }
+ 
+  createAccount(details){
+ 
+    return new Promise((resolve, reject) => {
+ 
+        let headers = new Headers();
+        headers.append('Content-Type', 'application/json');
+ 
+        this.http.post('https://YOUR_HEROKU_APP.herokuapp.com/api/auth/register', JSON.stringify(details), {headers: headers})
+          .subscribe(res => {
+ 
+            let data = res.json();
+            this.token = data.token;
+            this.storage.set('token', data.token);
+            resolve(data);
+ 
+          }, (err) => {
+            reject(err);
+          });
+ 
+    });
+ 
+  }
+ 
+  login(credentials){
+ 
+    return new Promise((resolve, reject) => {
+ 
+        let headers = new Headers();
+        headers.append('Content-Type', 'application/json');
+ 
+        this.http.post('https://YOUR_HEROKU_APP.herokuapp.com/api/auth/login', JSON.stringify(credentials), {headers: headers})
+          .subscribe(res => {
+ 
+            let data = res.json();
+            this.token = data.token;
+            this.storage.set('token', data.token);
+            resolve(data);
+ 
+            resolve(res.json());
+          }, (err) => {
+            reject(err);
+          });
+ 
+    });
+ 
+  }
+ 
+  logout(){
+    this.storage.set('token', '');
+  }
+ 
 }
